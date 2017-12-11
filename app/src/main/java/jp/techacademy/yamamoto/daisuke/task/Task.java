b0ffdcc8c12cd430198eb978b7a010690bb0246d
d_yamamoto <nice_day81@yahoo.co.jp>
2017-12-11 17:30:42 +0900

    参考に保管

---
+++ app/src/main/java/jp/techacademy/yamamoto/daisuke/task/Task.java
@@ -0,0 +1,49 @@
+package jp.techacademy.yamamoto.daisuke.task;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Task extends RealmObject implements Serializable {
+    private String title; // タイトル
+    private String contents; //　中身
+    private Date date; //　日時
+
+    // Set the id as a primary key
+    @PrimaryKey
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
